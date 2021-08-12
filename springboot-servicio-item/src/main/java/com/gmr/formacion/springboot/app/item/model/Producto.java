ae824264ab1270b5ff82c5f82d6cb7f29acd2964
Gregorio Mena Rodriguez <gmena@hiberus.com>
2021-08-12 13:29:04 +0200

    Añadir el puerto al Producto para poder ver qué instancia contesta y validar el balanceo

--- springboot-servicio-item/src/main/java/com/gmr/formacion/springboot/app/item/model/Producto.java
+++ springboot-servicio-item/src/main/java/com/gmr/formacion/springboot/app/item/model/Producto.java
@@ -8,28 +8,37 @@ public class Producto {
 	private String nombre;
 	private Double precio;
 	private Date createAt;
+	private Integer port;
 
 	public Producto() {
 	}
 
-	public Producto(Long id, String nombre, Double precio, Date createAt) {
+	public Producto(Long id, String nombre, Double precio, Date createAt, Integer port) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
 		this.precio = precio;
 		this.createAt = createAt;
+		this.port = port;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public Double getPrecio() {
 		return precio;
 	}
+
 	public Date getCreateAt() {
 		return createAt;
+	}
+
+	public Integer getPort() {
+		return port;
 	}
 }
--- springboot-servicio-productos/src/main/java/com/gmr/formacion/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/gmr/formacion/springboot/app/productos/controllers/ProductoController.java
@@ -1,8 +1,10 @@
 package com.gmr.formacion.springboot.app.productos.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +17,22 @@ public class ProductoController {
 
 	@Autowired
 	private IProductoService productoService;
-	
+
+	@Autowired
+	private Environment environment;
+
 	@GetMapping("/listar")
-	public List<Producto> listar(){
-		return productoService.finAll();
+	public List<Producto> listar() {
+		return productoService.finAll().stream().map(iterProducto -> {
+			iterProducto.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+			return iterProducto;
+		}).collect(Collectors.toList());
 	}
-	
+
 	@GetMapping("/detalle/{id}")
 	public Producto detalle(@PathVariable Long id) {
-		return productoService.findById(id);
+		Producto producto = productoService.findById(id);
+		producto.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		return producto;
 	}
 }
-
