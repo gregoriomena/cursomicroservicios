45042abfae5a6b0e5d50a7e4553d235626b43218
Gregorio Mena Rodriguez <gmena@hiberus.com>
2021-08-12 21:26:59 +0200

    Gestión de errores con Hystrix

--- springboot-servicio-item/src/main/java/com/gmr/formacion/springboot/app/item/controller/ItemController.java
+++ springboot-servicio-item/src/main/java/com/gmr/formacion/springboot/app/item/controller/ItemController.java
@@ -1,5 +1,6 @@
 package com.gmr.formacion.springboot.app.item.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gmr.formacion.springboot.app.item.model.Item;
+import com.gmr.formacion.springboot.app.item.model.Producto;
 import com.gmr.formacion.springboot.app.item.model.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
@@ -17,14 +20,29 @@ public class ItemController {
 	@Autowired
 	@Qualifier("itemServiceRestTemplate")
 	private ItemService itemService;
-	
+
 	@GetMapping("listar")
 	public List<Item> listar(){
 		return itemService.findAll();
 	}
-	
+
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("detalle/{id}/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad){
 		return itemService.findById(id, cantidad);
 	}
+
+	public Item metodoAlternativo(Long id, Integer cantidad){
+
+		// Podría intentar comunicar con otro servicio
+
+
+		Producto producto = new Producto(id, "Test error", 0.0, new Date(), 9999);
+		Item item = new Item(producto, cantidad);
+
+
+		return item;
+	}
+
+
 }
