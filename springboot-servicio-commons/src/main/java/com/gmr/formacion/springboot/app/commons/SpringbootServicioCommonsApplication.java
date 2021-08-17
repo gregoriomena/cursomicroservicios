98cfc3efadc0efc4fa183230575afb173bdd7e81
gregoriomena <gregoriomena@hiberus.com>
2021-08-17 12:04:03 +0200

    Al ser un proyecto de librería, en SpringbootServicioCommonsApplication quitamos el método main, ya que no arrancará una aplicación

--- springboot-servicio-commons/src/main/java/com/gmr/formacion/springboot/app/commons/SpringbootServicioCommonsApplication.java
+++ springboot-servicio-commons/src/main/java/com/gmr/formacion/springboot/app/commons/SpringbootServicioCommonsApplication.java
@@ -1,13 +1,7 @@
 package com.gmr.formacion.springboot.app.commons;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootServicioCommonsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootServicioCommonsApplication.class, args);
-	}
-
 }
