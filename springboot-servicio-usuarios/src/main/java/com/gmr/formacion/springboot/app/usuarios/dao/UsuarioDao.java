a463d5d0d7af4aa66ec32a52a83b470347d75962
gregoriomena <gregoriomena@hiberus.com>
2021-08-25 11:25:11 +0200

    Añado de forma dinámica los endpoint para permitir un CRUD de usuarios añadiendo la dependencia Rest Repositories y anotando el DAO

--- springboot-servicio-usuarios/src/main/java/com/gmr/formacion/springboot/app/usuarios/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/gmr/formacion/springboot/app/usuarios/dao/UsuarioDao.java
@@ -1,9 +1,11 @@
 package com.gmr.formacion.springboot.app.usuarios.dao;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.gmr.formacion.springboot.app.usuarios.models.entity.Usuario;
 
+@RepositoryRestResource(path = "usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
 
 	Usuario findByUsername(String username);
