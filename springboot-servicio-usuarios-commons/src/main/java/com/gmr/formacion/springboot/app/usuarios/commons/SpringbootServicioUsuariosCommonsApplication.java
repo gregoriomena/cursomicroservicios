e06b6b7a60ddcce3c5cac6b9717fed86cc7a01b0
gregoriomena <gregoriomena@hiberus.com>
2021-08-25 13:18:37 +0200

    Proyecto base para compartir el modelo de Usuario y Rol. Incluye la dependencia Spring JPA

---
+++ springboot-servicio-usuarios-commons/src/main/java/com/gmr/formacion/springboot/app/usuarios/commons/SpringbootServicioUsuariosCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.gmr.formacion.springboot.app.usuarios.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioUsuariosCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioUsuariosCommonsApplication.class, args);
+	}
+
+}
