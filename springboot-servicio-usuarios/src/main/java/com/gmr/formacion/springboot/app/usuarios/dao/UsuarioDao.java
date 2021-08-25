2af45c3ac681494cfe5b54470feb41ab265818b8
gregoriomena <gregoriomena@hiberus.com>
2021-08-25 13:05:33 +0200

    Configuración para renombrar el endpoint que busca por usuario y en vez de usar el que le asigna por defecto poder definirlo. Por defecto se usaría como api/usuarios/usuarios/search/findByUsername?username=gmena

--- springboot-servicio-usuarios/src/main/java/com/gmr/formacion/springboot/app/usuarios/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/gmr/formacion/springboot/app/usuarios/dao/UsuarioDao.java
@@ -1,13 +1,16 @@
 package com.gmr.formacion.springboot.app.usuarios.dao;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.gmr.formacion.springboot.app.usuarios.models.entity.Usuario;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
 
-	Usuario findByUsername(String username);
+	@RestResource(path = "buscar-username")
+	Usuario findByUsername(@Param("nombre") String username);
 
 }
