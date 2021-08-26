0c888a6746909b7839b2e2a74fa4a23a6a6d3f25
gregoriomena <gregoriomena@hiberus.com>
2021-08-26 19:28:37 +0200

    Cambio los inserts iniciales para usar la password encriptada

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellidos, email) VALUES ('gmena', '12345', 1, 'Gregorio', 'Mena Rodríguez', 'gmena@hiberus.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellidos, email) VALUES ('pperez', '12345', 1, 'Paco', 'Perez Perez', 'pperez@hiberus.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellidos, email) VALUES ('jgasrcia', '12345', 1, 'Juan', 'Garcia Perez', 'jgarcia@hiberus.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellidos, email) VALUES ('gmena', '$2a$10$iukn9Z1OoYlhc91sa0b3jOyD/fg9Z/fgIN70Wldad8HXXUNP2eEdW', 1, 'Gregorio', 'Mena Rodríguez', 'gmena@hiberus.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellidos, email) VALUES ('pperez', '$2a$10$C5vDoin.h5qNN4IsLrWTLuhrcbzSKPypVDqxQyI/VeXcsv4laAxzq', 1, 'Paco', 'Perez Perez', 'pperez@hiberus.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellidos, email) VALUES ('jgasrcia', '$2a$10$ZZbyFJ0V.byoUgyTWTgrxelo/K8.IstWiI7G8N1.5fdJKTdMn/YGO', 1, 'Juan', 'Garcia Perez', 'jgarcia@hiberus.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
