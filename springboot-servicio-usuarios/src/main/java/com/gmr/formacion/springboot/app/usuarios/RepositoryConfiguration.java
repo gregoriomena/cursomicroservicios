ae7c104c5749a5595c491ada800ce285aabdef49
gregoriomena <gregoriomena@hiberus.com>
2021-08-26 10:26:38 +0200

    Elimino el modelo de usuarios y roles del proyecto usuarios para usar el de la librería

--- springboot-servicio-usuarios/src/main/java/com/gmr/formacion/springboot/app/usuarios/RepositoryConfiguration.java
+++ springboot-servicio-usuarios/src/main/java/com/gmr/formacion/springboot/app/usuarios/RepositoryConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-import com.gmr.formacion.springboot.app.usuarios.models.entity.Usuario;
+import com.gmr.formacion.springboot.app.commons.usuarios.models.entity.Usuario;
 
 @Configuration
 public class RepositoryConfiguration implements RepositoryRestConfigurer {
--- springboot-servicio-usuarios/src/main/java/com/gmr/formacion/springboot/app/usuarios/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/gmr/formacion/springboot/app/usuarios/dao/UsuarioDao.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import com.gmr.formacion.springboot.app.usuarios.models.entity.Usuario;
+import com.gmr.formacion.springboot.app.commons.usuarios.models.entity.Usuario;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
