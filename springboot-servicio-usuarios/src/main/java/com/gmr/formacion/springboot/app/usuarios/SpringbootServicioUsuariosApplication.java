c4f0662c8900e3e238667b6b36070843dbda2f6b
gregoriomena <gregoriomena@hiberus.com>
2021-08-26 10:31:18 +0200

    Configuración para que se escanee el nuevo paquete con el modelo de Usuarios y Roles

--- springboot-servicio-usuarios/src/main/java/com/gmr/formacion/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
+++ springboot-servicio-usuarios/src/main/java/com/gmr/formacion/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
@@ -2,7 +2,9 @@ package com.gmr.formacion.springboot.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan({"com.gmr.formacion.springboot.app.commons.usuarios.models.entity"})
 @SpringBootApplication
 public class SpringbootServicioUsuariosApplication {
 
