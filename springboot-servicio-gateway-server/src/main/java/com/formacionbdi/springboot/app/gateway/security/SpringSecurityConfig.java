73171e99085352a6fdf4ccbfb66a63f550450f50
gregoriomena <gregoriomena@hiberus.com>
2021-09-27 10:55:36 +0200

    Dando reglas de seguridad a nuestras rutas de spring

--- springboot-servicio-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/security/SpringSecurityConfig.java
+++ springboot-servicio-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/security/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.formacionbdi.springboot.app.gateway.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -11,6 +12,15 @@ public class SpringSecurityConfig {
 	@Bean
 	public SecurityWebFilterChain configure(ServerHttpSecurity http) {
 		return http.authorizeExchange()
+				.pathMatchers("/api/security/oauth/**").permitAll()
+				.pathMatchers(HttpMethod.GET,
+						"/api/productos/listar",
+						"/api/items/listar",
+						"/api/usuarios/usuarios",
+						"/api/productos/detalle/{id}",
+						"/api/items/detalle/{id}/{cantidad}").permitAll()
+				.pathMatchers(HttpMethod.GET, "/api/usarios/usuarios/{id}").hasAnyRole("ADMIN", "USER")
+				.pathMatchers("/api/productos/**", "/api/items/**", "/api/usuarios/usuarios/**").hasRole("ADMIN")
 				.anyExchange().authenticated() // Cualquier ruta debe estar autentificada
 				.and().csrf().disable() // Esto sería para vistas-formularios y nosotros sólo trabajamos con API REST
 				.build();
