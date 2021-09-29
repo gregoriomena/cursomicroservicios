7e2da9dff0762da17660bbf399c8fff71b1f4d4b
gregoriomena <gregoriomena@hiberus.com>
2021-09-29 10:54:38 +0200

    Configurando MySQL Storage en Zipkin Server

--- zipkin.cmd
+++ zipkin.cmd
@@ -1,3 +1,6 @@
 @echo off
 set RABBIT_ADDRESSES=localhost:5672
+set STORAGE_TYPE=mysql
+set MYSQL_USER=zipkin
+set MYSQL_PASS=zipkin123456
 java -jar C:\Users\gmena\Downloads\zipkin-server-2.23.4-exec.jar
